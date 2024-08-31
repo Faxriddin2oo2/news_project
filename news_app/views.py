cfe3391fdc4f98c7dcd874b1f5d23ca9c7ad0cda
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-08-31 20:59:54 +0500

    News list va detail page

--- news_app/views.py
+++ news_app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from lib2to3.fixes.fix_input import context
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import News, Category
+
+def news_list(request):
+    news_list = News.published.all() # 1 chi usul
+    # news_list = News.objects.filter(status=News.Status.Published) # 2 chi usul
+    context = {
+        "news_list":news_list
+    }
+    return render(request, "news/news_list.html", context)
+
+def news_detail(request, id):
+    news = get_object_or_404(News, id=id, status=News.Status.Published)
+    context = {
+        "news":news
+    }
+
+    return render(request, 'news/news_detailed.html', context)
