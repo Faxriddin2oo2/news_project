5b46c8d7a3ec9a131326ca61127832d0c3bcaf36
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-09-01 17:54:32 +0500

    Yangiliklar sayti shablonini Djangoʼga oʻrnatish

--- news_app/views.py
+++ news_app/views.py
@@ -1,9 +1,6 @@
 from lib2to3.fixes.fix_input import context
-from msilib.schema import ListView
 
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
-
 from .models import News, Category
 
 def news_list(request):
@@ -13,10 +10,6 @@ def news_list(request):
         "news_list":news_list
     }
     return render(request, "news/news_list.html", context)
-# class NewsListView(ListView):
-#     model = News
-#     template_name = 'news/news_list.html'
-#     context_object_name = 'news_list'
 
 def news_detail(request, id):
     news = get_object_or_404(News, id=id, status=News.Status.Published)
@@ -25,6 +18,13 @@ def news_detail(request, id):
     }
 
     return render(request, 'news/news_detailed.html', context)
-# class NewsDetailView(DetailView):
-#     model = News
-#     template_name = "news/news_detailed.html"
+
+def homePageView(request):
+    news = News.published.all()
+    categories = Category.objects.all()
+    context = {
+        'news' : news,
+        "categories" : categories
+    }
+
+    return render(request, 'news/home.html', context)
