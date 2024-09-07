a2c6846b30cc0cbdc7b38c23413420a3637a6dea
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-09-07 19:01:13 +0500

    Yangiliklarni tahrirlash va oʻchirish funksiyalari  part 2

--- news_app/urls.py
+++ news_app/urls.py
@@ -3,12 +3,14 @@ from tkinter.font import names
 
 from django.urls import path
 from .views import news_list, news_detail, Page404View, aboutPageView, ContactPageView, HomePageView, \
-    LocalNewsView, ForeignNewsView, TechnologyNewsView, SportNewsView
+    LocalNewsView, ForeignNewsView, TechnologyNewsView, SportNewsView, NewsUpdateView, NewsDeleteView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name = 'home_page'),
     path('news/', news_list, name = "all_news_list"),
     path('news/<slug:news>/', news_detail, name = "news_detail_page"),
+    path('news/<slug>/edit/', NewsUpdateView.as_view(), name = "news_update"),
+    path('news/<slug>/delete/', NewsDeleteView.as_view(), name = "news_delete"),
     path('contact-us/', ContactPageView.as_view(), name = 'contact_page'),
     path('404/', Page404View, name = '404_page'),
     path('about/', aboutPageView, name = 'about_page' ),
