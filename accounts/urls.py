2913a831f982228149e9ab9bb2425d6f60a4138e
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-09-08 22:24:28 +0500

    Foydalanuvchi parolini oʻzgartirish

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from tkinter.font import names
 
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView,\
+    PasswordChangeView, PasswordChangeDoneView
 from .views import user_login, dashboard_view
 
 
@@ -9,5 +10,7 @@ urlpatterns = [
     # path('login/', user_login, name='login'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(http_method_names=['get', 'post', 'options']), name='logout'),
+    path('password-change/', PasswordChangeView.as_view(), name='password_change'),
+    path('password-change-done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('profile/', dashboard_view, name='user_profile'),
 ]
