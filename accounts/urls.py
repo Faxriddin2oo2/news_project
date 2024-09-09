f97f732810dce42a00915580054a3ceb25397534
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-09-09 18:39:02 +0500

    Foydalanuvchi parolini qayta tiklash. 1-qism

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,8 @@ from tkinter.font import names
 
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView,\
-    PasswordChangeView, PasswordChangeDoneView
+    PasswordChangeView, PasswordChangeDoneView, PasswordResetView, \
+    PasswordResetConfirmView, PasswordResetDoneView, PasswordResetCompleteView
 from .views import user_login, dashboard_view
 
 
@@ -12,5 +13,9 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(http_method_names=['get', 'post', 'options']), name='logout'),
     path('password-change/', PasswordChangeView.as_view(), name='password_change'),
     path('password-change-done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset/complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('profile/', dashboard_view, name='user_profile'),
 ]
