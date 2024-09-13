e38bc5d84f07c0e5cd7adabada966ea05509ece8
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-09-13 16:29:47 +0500

    wrote class NewsDetailView instead of function

--- news_app/urls.py
+++ news_app/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
 
-from .views import news_list, Page404View, aboutPageView, ContactPageView, HomePageView, \
+from .views import news_list, news_detail, Page404View, aboutPageView, ContactPageView, HomePageView, \
     LocalNewsView, ForeignNewsView, TechnologyNewsView, SportNewsView, NewsUpdateView, NewsDeleteView, \
-    NewsCreateView, admin_page_view, NewsDetailView
+    NewsCreateView, admin_page_view
 
 urlpatterns = [
     path('', HomePageView.as_view(), name = 'home_page'),
     path('news/', news_list, name = "all_news_list"),
     path('news/create/', NewsCreateView.as_view(), name = 'news_create' ),
-    # path('news/<slug:news>/', news_detail, name = "news_detail_page"),
-    path('news/<slug:slug>/', NewsDetailView.as_view(), name="news_detail_page"),
+    path('news/<slug:news>/', news_detail, name = "news_detail_page"),
     path('news/<slug>/edit/', NewsUpdateView.as_view(), name = "news_update"),
     path('news/<slug>/delete/', NewsDeleteView.as_view(), name = "news_delete"),
     path('contact-us/', ContactPageView.as_view(), name = 'contact_page'),
